25f614054f63bd48b44b81ee49f4e9ad434b2ce5
rkoeller224 <xavierk123@gmail.com>
2019-09-26 00:37:53 -0400

    Initial commit

---
+++ src/main/java/weekendhomework/main.java
@@ -0,0 +1,43 @@
+package weekendhomework;
+
+
+import java.util.Arrays;
+
+public class main {
+//    public static void main(String[] args) {
+
+
+
+
+
+//        System.out.println("THE ODD NUMBERS ARE");
+//        for (int x = 1; x <= 19; x++) {
+//            if (x % 2 != 0)
+//                System.out.println(x + "");
+//            }
+//        }
+
+
+
+
+
+//    public static void squares() {
+//        for (int x = 1; x <= 10; x++) {
+//            System.out.println(x * x);
+//        }
+//        return;
+//    }
+//
+//    public static void main(String[] args) {
+//        squares();
+//    }
+
+
+//    public class food {
+//
+//        public static void food(String[] args) {
+//            String[] breakfeast  = { "Sausage", "Eggs", "Beans", "Bacon", "Tomatoes", "Mushrooms" };
+//        System.out.println(Arrays.toString(breakfeast));
+//        }
+//    }
+}
